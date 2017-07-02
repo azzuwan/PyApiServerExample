807a05edd27cc28552bd83ccefa94facac4939de
Azzuwan Aziz <azzuwan@gmail.com>
2017-07-03 00:51:29 +0800

    Connection error troubleshooting

--- server.py
+++ server.py
@@ -20,9 +20,8 @@ app = Application()
 app.router.add_route('/', index)
 app.router.add_route('/articles', articles)
 app.router.add_route('/articles/keywords/{keywords}', keywords)
-rint("FFFFFFFFFF")
+print("FFFFFFFFFF")
 host = 'mongodb://aws-ap-southeast-1-portal.0.dblayer.com:15501/news'
 connect(db='news',host=host, username='azzuwan', password='Reddoor74', alias='default')
 app.run(debug=True)
 # host = 'mongodb://azzuwan:Reddoor74@aws-ap-southeast-1-portal.0.dblayer.com:15501/news'
-p
