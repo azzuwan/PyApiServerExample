ac0f94a18feb411a69700309a4fc459854a2f241
Azzuwan Aziz <azzuwan@gmail.com>
2017-07-03 01:07:55 +0800

    Connection error troubleshooting

--- server.py
+++ server.py
@@ -12,7 +12,8 @@ def articles(req):
 	return req.Response(text=docs.to_json())
 
 def keywords(req):
-	words =  req.match_dict["keywords"]
+	words =  req.match_dict['keywords']
+	print("FFFFFFFFFF: " + words)
 	docs = article_service.keywords(words)
 	return req.Response(text=docs.to_json(), headers={"Content-Type", "application/json"})
 
@@ -20,7 +21,7 @@ app = Application()
 app.router.add_route('/', index)
 app.router.add_route('/articles', articles)
 app.router.add_route('/articles/keywords/{keywords}', keywords)
-print("FFFFFFFFFF")
+
 # host = 'mongodb://azzuwan:Reddoor74@aws-ap-southeast-1-portal.0.dblayer.com:15501/news'
 host = 'mongodb://aws-ap-southeast-1-portal.0.dblayer.com/news'
 connect(db='news',host=host, port=15501, username='azzuwan', password='Reddoor74', alias='default', connect=False)
