78ccb27b7ba49bab9e0d68b2df572308c714d5ca
Azzuwan Aziz <azzuwan@gmail.com>
2017-07-03 00:10:37 +0800

    Change connection settings

--- models/news.py
+++ models/news.py
@@ -1,6 +1,7 @@
 from mongoengine import *
 
-host = 'mongodb://azzuwan:Reddoor74@aws-ap-southeast-1-portal.2.dblayer.com:15501,aws-ap-southeast-1-portal.0.dblayer.com:15501/news'
+# host = 'mongodb://azzuwan:Reddoor74@aws-ap-southeast-1-portal.2.dblayer.com:15501,aws-ap-southeast-1-portal.0.dblayer.com:15501/news'
+host = 'mongodb://azzuwan:Reddoor74@aws-ap-southeast-1-portal.0.dblayer.com:15501/news'
 connect(host=host)
 
 class Articles(Document):
