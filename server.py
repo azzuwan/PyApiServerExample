5ae77fd4b9b4096f3af1c8227f076c1ff9a009fe
Azzuwan Aziz <azzuwan@gmail.com>
2017-07-03 10:25:59 +0800

    Increase request buffer as workaround for stackoverflow in asyncio

--- server.py
+++ server.py
@@ -14,8 +14,13 @@ def articles(req):
 	"""
 	Get alll articles
 	"""
-	#AsyncIO buffer problem
+	#AsyncIO transport buffer problem
+	b_size = req.get_write_buffer_limits()
+	print("Transport Buffer Size: " + b_size)
 	req.transport.set_write_buffer_limits(high=16384)
+	b_size = req.get_write_buffer_limits()
+	print("Transport Buffer Size After: " + b_size)
+
 
 	docs = article_service.all()	
 	return req.Response(text=docs.to_json())
@@ -25,7 +30,11 @@ def keywords(req):
 	Retrieve articles by keywords
 	"""
 	#AsyncIO buffer problem
+	b_size = req.get_write_buffer_limits()
+	print("Transport Buffer Size: " + b_size)
 	req.transport.set_write_buffer_limits(high=16384)
+	b_size = req.get_write_buffer_limits()
+	print("Transport Buffer Size Afteer: " + b_size)
 
 	words =  req.match_dict['keywords']	
 	docs = article_service.keywords(words)	
